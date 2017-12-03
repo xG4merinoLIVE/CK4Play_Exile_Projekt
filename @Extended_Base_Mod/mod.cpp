770f0329dcdd4169dc9c25eaa88646f019271ea3
xG4merinoLIVE <MindChaoz@live.de>
2017-12-03 09:51:51 +0100

    CK4Play Exile Server
    Die 1.0 Version des CK4Play Community Exile Server ^^ [ ONLY PVE ]

---
+++ @Extended_Base_Mod/mod.cpp
@@ -0,0 +1,5 @@
+name = "Extended Base Mod";
+actionName = "EBM";
+version = "0.3.3";
+picture = "extendedbase.paa";
+author = "Freakylein";
---
+++ @Fox_SurvivalCars/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 933985898;
+name = "Fox_SurvivalCars";
+timestamp = 5248028537126272534;
