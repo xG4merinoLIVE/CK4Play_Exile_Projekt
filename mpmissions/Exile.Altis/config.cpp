cf0a3a7f0f66c365c7c8812df36f0d40bc0c36cd
xG4merinoLIVE <MindChaoz@live.de>
2018-01-08 00:15:57 +0100

    Update vom Hauptserver 1.1.13
    + Bandagen nun kaufbar mit low Respect

--- mpmissions/Exile.Altis/config.cpp
+++ mpmissions/Exile.Altis/config.cpp
@@ -683,7 +683,7 @@ class CfgExileArsenal
 	///////////////////////////////////////////////////////////////////////////////
 	class Exile_Item_InstaDoc                       { quality = 5; price = 1250; };
 	class Exile_Item_Vishpirin						{ quality = 3; price = 300; };
-	class Exile_Item_Bandage	                    { quality = 2; price = 100; };
+	class Exile_Item_Bandage	                    { quality = 1; price = 100; };
 	class Exile_Item_Heatpack	                    { quality = 1; price = 50; };
 	class Exile_Item_Defibrillator					{ quality = 1; price = 1500; };
 
